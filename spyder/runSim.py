64111c5fe13099d62c3c5fc5d01b9081d12af560
Hiroaki Imoto <himoto@protein.osaka-u.ac.jp>
2019-02-03 15:25:09 +0900

    Remove .ipynb

--- spyder/runSim.py
+++ spyder/runSim.py
@@ -3,6 +3,6 @@
 %run -i ../model/setVarEnum.py
 %run -i ../model/diffeq.py
 %run -i ../model/initialValues.py
-%run -i ../runSim.py
-%run -i ../plot.py
+%run -i ../simulation.py
+%run -i ../plotFunc.py
 plt.savefig('./TAK1IKKmodel.png',bbox_inches='tight')

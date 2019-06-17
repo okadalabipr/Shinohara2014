3a1f368d6c75a77d4f32eb5570ddbaaf263bfac2
himoto <himoto@protein.osaka-u.ac.jp>
2019-06-17 13:09:32 +0900

    Create runSim.py

---
+++ runSim.py
@@ -0,0 +1,18 @@
+import glob
+
+def include(pyfile):
+    with open(pyfile,'r',encoding='utf-8') as f:
+        script = f.read()
+        exec(script,globals())
+
+
+include('model/f_parameter.py')
+include('model/f_variable.py')
+include('model/differential_equation.py')
+include('model/initial_condition.py')
+
+include('simulation.py')
+include('plot_func.py')
+
+# plt.savefig('./TAK1IKKmodel.png',bboc_inches='tight')
+plt.show()
